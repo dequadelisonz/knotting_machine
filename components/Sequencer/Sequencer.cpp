cbf7be3d26d8a6cc14471ec2bf1fd489d46cb882
Paolo Martinello <paolomartinello76@gmail.com>
2023-04-14 21:58:36 +0200

    1.4.2 Added dummy extraction in sequencer parser

--- components/Sequencer/Sequencer.cpp
+++ components/Sequencer/Sequencer.cpp
@@ -29,32 +29,34 @@ void Sequencer::parse()
 
     while ((row != NULL) && !isEnd)
     {
-        //printf("id: %s\n", row);
+        // printf("id: %s\n", row);
         id = atoi(row);
 
         row = strtok(NULL, DELIMITER); // extracting group duration
-        //printf("Duration: %s\n", row);
+        // printf("Duration: %s\n", row);
         groupDuration = atof(row);
 
         row = strtok(NULL, DELIMITER); // extracting offset
-        //printf("Offset: %s\n", row);
+        // printf("Offset: %s\n", row);
         offset = atof(row);
 
         row = strtok(NULL, DELIMITER); // extracting description
-        //printf("Descr. %s\n", row);
+        // printf("Descr. %s\n", row);
         strncpy(description, row, Pass::MAX_CHAR_DESCR - 1);
 
         row = strtok(NULL, DELIMITER); // extracting duration
-        //printf("Duration: %s\n", row);
+        // printf("Duration: %s\n", row);
         duration = atof(row);
 
         row = strtok(NULL, DELIMITER); // extracting pin
-        //printf("Pin: %s\n", row);
+        // printf("Pin: %s\n", row);
         pin = atoi(row);
 
         row = strtok(NULL, DELIMITER); // extracting status
-        //printf("Status: %s\n", row);
+        // printf("Status: %s\n", row);
         status = (atoi(row) == 1) ? true : false;
+
+        row = strtok(NULL, DELIMITER); // TODO dummy extraction for triggering pin
 
         Pass pass(id,
                   groupDuration,
@@ -71,8 +73,8 @@ void Sequencer::parse()
     _totalDuration = groupDuration + offset;
     _cycle._logContent();
 
-    //priming cycle index - set position to last pass (_lastId-1) and then advance to get first position
-    _curPassIndex=_cycle._lastId-1;
+    // priming cycle index - set position to last pass (_lastId-1) and then advance to get first position
+    _curPassIndex = _cycle._lastId - 1;
     advance();
 }
 
@@ -86,12 +88,12 @@ bool const Sequencer::advance()
         _curPassIndex = 0U;
         _isAtBegin = true;
     }
-    _curId=_cycle._passes[_curPassIndex].getId();
+    _curId = _cycle._passes[_curPassIndex].getId();
     _curDuration = _cycle._passes[_curPassIndex].getDuration();
     _curOffset = _cycle._passes[_curPassIndex].getOffset();
-    strcpy( _curDecription,_cycle._passes[_curPassIndex].getDescription());
+    strcpy(_curDecription, _cycle._passes[_curPassIndex].getDescription());
     _curPin = _cycle._passes[_curPassIndex].getPin();
     _curStatus = _cycle._passes[_curPassIndex].getStatus();
 
-    return  _isAtBegin;
+    return _isAtBegin;
 }
